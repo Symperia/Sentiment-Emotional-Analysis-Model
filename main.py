5f0ed1c36fa495bc4489e8b2135bb4396bcfe426
Symperia <bastian7370@gmail.com>
2026-05-21 21:17:10 -0500

    changed emotional analysis pre-etrained llm and checked SD of emotion values

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from transformers import pipeline
+import numpy as np
 import time
 import parsing
 
@@ -9,7 +10,7 @@ stats = {"Simple": 0, "Complex": 0}
 
 # model loading
 sentiment_analysis = pipeline("sentiment-analysis", model="cardiffnlp/twitter-roberta-base-sentiment-latest")
-emotion_analysis = pipeline("text-classification", model="bhadresh-savani/distilbert-base-uncased-emotion")
+emotion_analysis = pipeline("text-classification", model="j-hartmann/emotion-english-distilroberta-base", top_k=None)
 
 # set file path
 file_path = input("Input file path as raw string.")
@@ -19,6 +20,19 @@ conversations = parsing.parse(file_path)
 conversations = conversations[3]
 sentiment = sentiment_analysis(conversations, truncation=True, max_length=512)
 emotion = emotion_analysis(conversations, truncation=True, max_length=512)
+
+#emotion standard deviation
+emotion_values=[]
+for i in range(6):
+    emotion_values.append(emotion[i]['label'])
+
+if np.std(emotion_values)>0.15:
+    emotional_significance = True
+else:
+     emotional_significance = False
+
+
+
 
 print(sentiment)
 print(emotion)
